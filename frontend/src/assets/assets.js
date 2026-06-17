72d539c9ca69d75dee5b684c76cc7c5ed9008255
Ravi Kiran <allimilliravikiran@gmail.com>
2026-06-17 13:17:23 +0530

    updated the hero section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,10 @@
 
 import logo from './logo.png'
 import profile_icon from './profile_icon.png'
+import hero from './hero.png'
 
 export const assets = {
     logo,
     profile_icon,
-
+    hero,
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
-        <Categories/>
     </div>
   )
 }
